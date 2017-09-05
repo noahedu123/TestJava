d822d03e45264fce7f107246163075e1cd0354db
noahedu123 <noahedu123@163.com>
2017-09-05 16:17:43 +0800

    Update test1.java

--- src/sunmutao/src/com/cn/test1.java
+++ src/sunmutao/src/com/cn/test1.java
@@ -4,6 +4,7 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("111");
 		System.out.println("hello ,word");
-		System.out.println("hello1 ,word");
+		
+		
 	}
 }
