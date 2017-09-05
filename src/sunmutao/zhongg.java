808bcbcf3c75833e75a2efad3b5f24d8a1ed5de1
noahedu123 <noahedu123@163.com>
2017-09-05 15:52:45 +0800

    1

--- src/sunmutao/zhongg.java
+++
@@ -1,7 +0,0 @@
-package sunmutao;
-
-public class zhongg {
-	public static void main(String[] args) {
-		System.out.println("heloodf");
-	}
-}
--- src/sunmutao/zhongguo.java
+++
@@ -1,7 +0,0 @@
-package sunmutao;
-
-public class zhongguo {
-	public static void main(String[] args) {
-		System.out.println("3343");
-	}
-}
