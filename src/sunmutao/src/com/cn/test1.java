b6e6c1168b47df293c734ad883db0d965fe40606
noahedu123 <noahedu123@163.com>
2017-09-05 16:30:54 +0800

    f
    ds

--- src/sunmutao/src/com/cn/test1.java
+++ src/sunmutao/src/com/cn/test1.java
@@ -2,7 +2,7 @@ package com.cn;
 
 public class test1 {
 	public static void main(String[] args) {
-		System.out.println("111");
+		System.out.println("4444");
 		System.out.println("hello ,word");
 		
 		
