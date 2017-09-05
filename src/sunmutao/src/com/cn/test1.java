b682f713f70b474256e21634c0963117666d7aab
noahedu123 <noahedu123@163.com>
2017-09-05 16:34:31 +0800

    dfsf

--- src/sunmutao/src/com/cn/test1.java
+++ src/sunmutao/src/com/cn/test1.java
@@ -4,7 +4,7 @@ public class test1 {
 	public static void main(String[] args) {
 		System.out.println("4444");
 		System.out.println("hello ,word");
-		String a ="dfsdfs";
+		String a ="ffffff";
 		
 	}
 }
